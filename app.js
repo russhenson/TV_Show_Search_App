547c814c7a35a5617a1c1c860e1ec053aecdbe6d
russhenson <49965783+russhenson@users.noreply.github.com>
2022-04-08 18:12:24 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+// https://www.tvmaze.com/api
+
+const form = document.querySelector('#searchForm');
+
+form.addEventListener('submit', async function(e) {
+    e.preventDefault();
+    const input = form.elements.query.value;
+    const config = { params: { q: input } }
+    const res = await axios.get(` https://api.tvmaze.com/search/shows?`, config);
+    makeImages(res.data);
+    form.elements.query.value = "";
+    form.addEventListener('submit', async function(e) {
+        e.preventDefault();
+        deleteResults();
+    });
+    
+});
+
+const deleteResults = () => {
+    const rowContainer = document.querySelector('#rowContainer');
+    while(rowContainer.firstChild) {
+        rowContainer.removeChild(rowContainer.firstChild)
+    }
+}
+
+const makeImages = (shows) => {
+
+    const imagesContainer = document.querySelector('#imagesContainer');
+    const rowContainer = document.querySelector('#rowContainer');
+
+    for(let result of shows){
+        if(result.show.image) {
+            const col = document.createElement('div')
+            const img = document.createElement('img');
+
+            col.classList.add("col", "imageResult");
+            img.src = result.show.image.medium;
+
+            col.append(img);
+            rowContainer.append(col);
+            imagesContainer.append(rowContainer);
+        }
+    }
+}
